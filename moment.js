7a06128e833700fe943281b37a51d696da1b8b58
anthony soon <anthonychansoon@gmail.com>
2019-09-26 15:26:46 -0700

    added object and semi fixed HTML layout

---
+++ moment.js
@@ -0,0 +1,39 @@
+let myVar = [
+{
+    time: "0900",
+    event: "event09",
+},
+{
+    time: "1000",
+    event: "event10",
+},
+{
+    time: "1100",
+    event: "event11",
+},
+{
+    time: "1200",
+    event: "event12",
+},
+{
+    time: "1300",
+    event: "event13",
+},
+{
+    time: "1400",
+    event: "event14",
+},
+{
+    time: "1500",
+    event: "event15",
+},
+{
+    time: "1600",
+    event: "event16",
+},
+{
+    time: "1700",
+    event: "event17",
+}];
+
+
